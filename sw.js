9cf8e0ce183687626469d64b8c85596cf5e978f7
happyk1900 <happyk1988love@gmail.com>
2026-06-29 16:25:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,40 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'kdrive-core-v2'; // Đổi số v2, v3... mỗi khi anh cập nhật code mới
+const urlsToCache = [
+  './',
+  './index.html',
+  './kdrive-style.css',
+  './core-logic.js',
+  './kdrive-icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Xóa sạch bộ nhớ đệm cũ
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
