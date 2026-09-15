11ddeb3a477785dda3ab78fcafe2cbb6fdeb8894
happyk1900 <happyk1988love@gmail.com>
2026-09-16 06:57:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,14 @@
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Đã cài đặt');
+    console.log('[Service Worker] Đã cài đặt');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('[Service Worker] Đã kích hoạt');
+    e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
-  // Dòng này là bùa chú bắt buộc phải có để Chrome cấp quyền cài PWA
+    // Chuyển tiếp tất cả request mạng đi bình thường để app load mượt mà
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
